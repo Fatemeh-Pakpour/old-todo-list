1bee6ba1fedec485841a5957d3f0849e81f4f891
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-10-18 16:28:30 +0200

    Updates

---
+++ precache-manifest.7f21250f44da0be2add9853f4a7e163d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baac7cc0b0af8dfecda6bca71741d03a",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "b54d0d5c0180ea1165da",
+    "url": "/my-project/static/css/main.4c11f3c2.chunk.css"
+  },
+  {
+    "revision": "6020a30cce082ec8ff86",
+    "url": "/my-project/static/js/2.82a79220.chunk.js"
+  },
+  {
+    "revision": "b54d0d5c0180ea1165da",
+    "url": "/my-project/static/js/main.8fcd6882.chunk.js"
+  },
+  {
+    "revision": "214dd81775f8be238553",
+    "url": "/my-project/static/js/runtime-main.5292cd14.js"
+  }
+]);
